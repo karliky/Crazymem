7408f422fb4836fa1f2854f107da70bc9592cab5
Karliky <karliky@gmail.com>
2020-09-11 18:12:59 +0200

    use build directory to output js code

--- src/lib.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Crazymem = void 0;
-var crazymem_1 = require("./crazymem");
-var Lib = function (process_name) { return new crazymem_1.Crazymem(process_name); };
-exports.Crazymem = Lib;

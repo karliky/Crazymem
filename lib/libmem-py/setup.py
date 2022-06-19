980771e368624f5a19e5f5a07d142f7e0fce5086
karliky <karliky@gmail.com>
2022-06-19 15:31:17 +0200

    remove submodule

---
+++ lib/libmem-py/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, Extension, find_packages
+from sys import platform
+import os
+
+src_dir = f"src{os.sep}libmem-py"
+libs = []
+readme = ""
+
+with open("README.md", "r") as f:
+	readme = f.read()
+	f.close()
+
+if platform == "win32":
+	libs.append("user32")
+	libs.append("psapi")
+elif platform.startswith("linux"):
+	libs.append("dl")
+elif platform.find("bsd") != -1:
+	libs.append("dl")
+	libs.append("kvm")
+	libs.append("procstat")
+	libs.append("elf")
+
+libmem = Extension(
+	name = "libmem",
+	include_dirs = [ src_dir ],
+	sources = [ f"{src_dir}{os.sep}libmem-py.c", f"{src_dir}{os.sep}libmem.c" ],
+	libraries = libs
+)
+
+setup(
+	name = "libmem",
+	version = "0.1.3",
+	description = "Process and Memory Hacking Library",
+	long_description = readme,
+	long_description_content_type = "text/markdown",
+	author = "rdbo",
+	url = "https://github.com/rdbo/libmem",
+	project_urls = {
+		"Bug Tracker" : "https://github.com/rdbo/libmem/issues",
+		"Discord Server" : "https://discord.com/invite/Qw8jsPD99X"
+	},
+	package_dir = { "" : "src" },
+	packages = find_packages(where="src"),
+	python_requires = ">=3.6",
+	ext_modules = [libmem]
+)
